495394d4f7940167008adc7c36cd4a9ef3dba821
Ahmed Mokhter <ahmedmokhttar159@gmail.com>
2023-01-23 01:47:55 +0200

    creating retrofit and init hit

---
+++ app/src/main/java/com/example/news/api/NewsAPIServices.kt
@@ -0,0 +1,30 @@
+package com.example.news.api
+
+import com.example.news.models.NewsResponse
+import com.example.news.util.Constant.Companion.API_KEY
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface NewsAPI {
+    @GET("v2/everything")
+    suspend fun getNews(
+        @Query("country")
+        countryCode:String="us",
+        @Query("page")
+        pageNumber:Int=1,
+        @Query("apiKey")
+        apiKey:String=API_KEY
+    ):Response<NewsResponse>
+
+    @GET("v2/everything")
+    suspend fun searchNews(
+        @Query("q")
+        searchQuery:String,
+        @Query("page")
+        pageNumber:Int=1,
+        @Query("apiKey")
+        apiKey:String=API_KEY
+    ):Response<NewsResponse>
+
+}
